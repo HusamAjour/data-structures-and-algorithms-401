d9c278c20a7aaaf591e82f7a00a63622201366c4
HusamAjour <husamaj278@gmail.com>
2021-02-07 20:19:13 +0200

    add hashtable

---
+++ challenges/hashtable/hashtable.js
@@ -0,0 +1,65 @@
+'use strict';
+const linkedList = require('./linked-list');
+
+class HashTable {
+  constructor(size) {
+    this.size = size;
+    this.table = new Array(size);
+  }
+
+  hash(key) {
+    const sumCharCode = key.split('').reduce((acc, char) => {
+      return acc + char.charCodeAt(0);
+    }, 0);
+    return (sumCharCode * 19) % this.size;
+  }
+
+  add(key, value) {
+    let hash = this.hash(key);
+    if (!this.table[hash]) {
+      this.table[hash] = new linkedList();
+    }
+    const entry = { [key]: value };
+    this.table[hash].append(entry);
+  }
+
+  get(key) {
+    let hash = this.hash(key);
+    if (this.table[hash]) {
+      let current = this.table[hash].head;
+      while (current.next) {
+        if (current.value[key]) {
+          return current.value[key];
+        }
+        current = current.next;
+      }
+    } else return false;
+  }
+
+  contains(key) {
+    let hash = this.hash(key);
+    if (this.table[hash]) {
+      let current = this.table[hash].head;
+      while (current.next) {
+        if (current.value[key]) {
+          return true;
+        }
+        current = current.next;
+      }
+    } else return false;
+  }
+}
+
+// const hashTable = new HashTable(1024);
+// hashTable.add('cat', 'Candy');
+// hashTable.add('act', 'Christian Bale');
+// hashTable.add('name', 'Husam');
+// hashTable.add('act', 'Husam');
+// hashTable.add('city', 'Aqaba');
+// hashTable.add('name', 'Hanaa');
+// console.log(hashTable.get('act'));
+// console.log(hashTable.get('cat'));
+// console.log(hashTable.contains('name'));
+
+module.exports = HashTable;
+
